9ea2a17025d27b5a09be2264dcb5c0a97461ade6
Faraz Patankar <farazpatankar@gmail.com>
2016-02-24 17:41:04 -0500

    Add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   has_many :games
   has_many :invites
+
+  validates :username, :email, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
