616b4c2afeee80cdc407c23e53073ce3defe97a9
Faraz Patankar <farazpatankar@gmail.com>
2016-03-01 14:36:56 -0500

    Replace placeholder images

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   has_many :requests
 
   validates :username, :email, presence: true
+  validates :username, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
