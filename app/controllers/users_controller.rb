723d37499ca6a9eb7713edc654f8531ee85aca82
Faraz Patankar <farazpatankar@gmail.com>
2016-03-21 16:41:06 -0400

    Fix minor bug with before_action on users show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+	before_action :authenticate_user!, only: [ "show" ]
 	def show
 		@user = User.find_by(id: params[:id])
 		@past_games = @user.games.where('start_time < ?', Time.zone.now)
